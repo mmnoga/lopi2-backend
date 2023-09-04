5550635f813b8b633a47ca1c68114a11bfa64c1b
Artur Dudzik <dudziaso@gmail.com>
2023-09-04 18:35:29 +0200

    disable cors in spring security

--- src/main/java/com/liftoff/project/configuration/SecurityConfig.java
+++ src/main/java/com/liftoff/project/configuration/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers
                         .frameOptions(frameOptions -> frameOptions
                                 .sameOrigin()))
