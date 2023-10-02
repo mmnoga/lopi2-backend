3fc18d030647b86978d27f77ba8448d729f2eafd
Marcin <91083137+mmnoga@users.noreply.github.com>
2023-10-02 19:17:19 +0200

    bugfix/L01-182: adjusted delivery and payment method initializers (#52)

---
+++ src/main/resources/db/migration/postgresql/R__insert_delivery_and_payment_methods.sql
@@ -0,0 +1,15 @@
+DELETE FROM DELIVERY_METHODS;
+
+DELETE FROM PAYMENT_METHODS;
+
+INSERT INTO DELIVERY_METHODS (NAME, DESCRIPTION, COST) VALUES
+       ('IN_POST', 'InPost', 10.0),
+       ('COURIER_SERVICE', 'Przesyłka kurierska', 20.0),
+       ('IN_STORE_PICKUP', 'Odbiór osobisty w sklepie stacjonarnym', 0.0);
+
+INSERT INTO PAYMENT_METHODS (NAME, DESCRIPTION) VALUES
+       ('BLIK', 'Blik'),
+       ('CREDIT_CARD', 'Karta kredytowa'),
+       ('BANK_TRANSFER', 'Przelew bankowy'),
+       ('ONLINE_TRANSFER', 'Szybki przelew online'),
+       ('COD', 'Płatność przy odbiorze');
