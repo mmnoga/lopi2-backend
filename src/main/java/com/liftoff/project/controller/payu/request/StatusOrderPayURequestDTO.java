a0a310c238a60e1a9145f8f736763303feaacf6b
genger76 <genger@wp.pl>
2023-11-23 13:40:10 +0100

    First commit

---
+++ src/main/java/com/liftoff/project/controller/payu/request/StatusOrderPayURequestDTO.java
@@ -0,0 +1,18 @@
+package com.liftoff.project.controller.payu.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatusOrderPayURequestDTO {
+
+    @NotNull(message = "OrderPayU Status Code is required")
+    private String statusCode;
+
+}
